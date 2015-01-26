58bc87681d9ea0199c3de6dddab75ac6e2d68acc
Travis Himes <travis.himes@gmail.com>
2015-01-26 11:54:46 -0500

    Now we show something! also updated the menu a little bit.

--- app/src/main/java/com/thimes/rxandroidexamples/MainActivity.java
+++ app/src/main/java/com/thimes/rxandroidexamples/MainActivity.java
@@ -4,20 +4,31 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    @InjectView(R.id.only_text_view) TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        resetTextView();
     }
 
+    private void resetTextView() {
+        textView.setText("Perhaps choose something from the menu?");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,19 +44,35 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if (id == R.id.action_simple_observable) {
-            createSimpleObservable();
-            return true;
+
+        switch(id) {
+            case R.id.action_clear:
+                resetTextView();
+                return true;
+            case R.id.action_simple_observable:
+                createSimpleObservable();
+                return true;
+            case R.id.action_observable_actions:
+                final StringBuilder sb = new StringBuilder();
+                createSimpleObservable().subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        sb.append("received ").append(integer).append("\n");
+                        textView.setText(sb.toString());
+                    }
+                });
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void createSimpleObservable() {
+    private Observable<Integer> createSimpleObservable() {
         Observable<Integer> simple =
                 Observable
                         .range(0, 10)
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread());
+        return simple;
     }
 }
